3ebbe3eaf99df8dadaa58e166cc1474f2793c393
Celdama <celdama.dev@gmail.com>
2022-01-07 15:15:30 +0100

    setup files for start

---
+++ src/GlobalStyle.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    max-width: 1280px;
+    --mainBlue: #a8dadc;
+  }
+
+  * {
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: var(--mainBlue);
+  }
+
+  .App {
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+  }
+`;
---
+++ src/components/Journal/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Wrapper } from './Journal.styles';
+
+const Journal = () => {
+  return <Wrapper>my journal</Wrapper>;
+};
+
+export default Journal;
